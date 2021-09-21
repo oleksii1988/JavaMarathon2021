252c189b2f9efd23f57951fc0cad3513a49fa26d
AlexLytvynenko <oilytvynenko@gmail.com>
2021-09-21 22:33:45 +0300

    Day 2 Task 2 "HomeWork"

--- src/main/java/day2/Task2.java
+++ src/main/java/day2/Task2.java
@@ -1,7 +1,24 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        int value1 = scanner.nextInt();
+        int value2 = scanner.nextInt();
+
+        if (value1 > value2) {
+            System.out.println("ERROR");
+        }
+
+        for (int i = value1; value2 > i; i++) {
+
+            if (i++ % 5 == 0 && i % 10 != 0) {
+
+                System.out.println(i);
+            }
+        }
     }
 }
