7fe8202f64d80c16357d8d4aa65a821f1293f98e
AlexLytvynenko <oilytvynenko@gmail.com>
2021-09-23 14:35:15 +0300

    Day 3 Task 2 "HomeWork"

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,23 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double value1 = scanner.nextDouble();
+        double value2 = scanner.nextDouble();
+
+
+        for (; value2 != 0; value1 = scanner.nextDouble(), value2 = scanner.nextDouble()) {
+
+            double result = value1 / value2;
+            System.out.println(result);
+
+        }
+
 
     }
 }
