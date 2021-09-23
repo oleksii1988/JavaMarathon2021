200aa725d9c0209603d2b25bcbdebdd9a12cdb55
AlexLytvynenko <oilytvynenko@gmail.com>
2021-09-23 22:09:55 +0300

    Day 3 Task 3 "HomeWork"

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,25 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        double value1 = scanner.nextDouble();
+        double value2 = scanner.nextDouble();
+
+
+        for(int i = 0; i<5;i++,value1= scanner.nextDouble(),value2= scanner.nextDouble()){
+            System.out.println(value1/value2);
+
+        if(value2==0){
+            System.out.println("Деление на ноль");
+        }
+
+        }
+
+
 
     }
 }
